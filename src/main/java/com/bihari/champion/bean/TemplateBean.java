da089ad78c3effa70ecb36be084bee299c61d5a6
biharitomi <biharitomi@gmail.com>
2012-09-26 00:45:53 +0200

    create second menu with dynamic menuitems

--- src/main/java/com/bihari/champion/bean/TemplateBean.java
+++ src/main/java/com/bihari/champion/bean/TemplateBean.java
@@ -1,9 +1,18 @@
 package com.bihari.champion.bean;
 
+
+import javax.annotation.PostConstruct;
+import javax.el.ELContext;
+import javax.el.ExpressionFactory;
+import javax.faces.application.Application;
 import javax.faces.bean.ManagedBean;
+import javax.faces.context.FacesContext;
 import javax.faces.event.ActionEvent;
 
+import org.primefaces.component.menuitem.MenuItem;
 import org.primefaces.event.TabChangeEvent;
+import org.primefaces.model.DefaultMenuModel;
+import org.primefaces.model.MenuModel;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -11,6 +20,12 @@ import org.slf4j.LoggerFactory;
 public class TemplateBean {
 	private static final Logger logger = LoggerFactory.getLogger(TemplateBean.class);
 	
+	private MenuModel menuModel;
+	
+	public MenuModel getMenuModel() {
+		return menuModel;
+	}
+
 	public void onChange(TabChangeEvent event){
 		logger.info("MENUITEM CLICKED!!!");
 	}
@@ -19,5 +34,45 @@ public class TemplateBean {
 		logger.info("MENUITEM CLICKED!!!");
 		System.out.println("AADSF");
 		
+	}
+	
+	public String menuItemClicked2(){
+		logger.info("DYNAMIC MENUITEM CLICKED!!!");
+		for(Object o:FacesContext.getCurrentInstance().getAttributes().keySet()){
+			logger.info("ATTRIBUTE: "+o.toString());
+		}
+		return "/views/kapcsolat.xhtml";
+	}
+	
+	
+	@PostConstruct
+	private void init(){
+		FacesContext facesContext = FacesContext.getCurrentInstance();
+        Application application = facesContext.getApplication();
+        ExpressionFactory expressionFactory = application.getExpressionFactory();
+        ELContext elContext = facesContext.getELContext();
+        
+		menuModel=new DefaultMenuModel();
+		
+		MenuItem item;
+		
+		item=new MenuItem();
+		item.setId("xxx1");
+		item.setValueExpression("value", expressionFactory.createValueExpression(elContext, "Home", String.class));
+        item.setActionExpression(expressionFactory.createMethodExpression(elContext, "#{templateBean.menuItemClicked2}", String.class, new Class[0]));
+        item.setAjax(false);
+        item.setAsync(false);
+        item.setUpdate(":dataForm");
+        menuModel.addMenuItem(item);
+        
+        item=new MenuItem();
+		item.setId("xxx2");
+		item.setValueExpression("value", expressionFactory.createValueExpression(elContext, "Referenciák", String.class));
+        item.setActionExpression(expressionFactory.createMethodExpression(elContext, "#{templateBean.menuItemClicked2}", String.class, new Class[0]));
+        
+        item.setAjax(false);
+        item.setAsync(false);
+        item.setUpdate(":dataForm");
+        menuModel.addMenuItem(item);
 	}
 }
